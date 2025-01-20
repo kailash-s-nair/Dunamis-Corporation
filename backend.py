f38c7dba6a42d1a3bb7ebeeb613267118c105f15
cloTech1 <113944156+cloTech1@users.noreply.github.com>
2025-01-20 15:14:40 -0500

    backend.py revamp
    - Various names more accurate and consistent.
    - Comments describing what things do.
    - Add function can now handle a csv file with any number of columns.
    - Delete function coded more sanely, and can delete several entries simultaneously.

--- backend.py
+++ backend.py
@@ -1,65 +1,101 @@
-#This is a test
-
 import csv
 
-def read_items(file):
+#Reads every item in a csv file.
+#file:  csv file name.
+def print_rows(file):
     with open(file, newline='') as fd:
-        iris_reader = csv.reader(fd, delimiter=',', quotechar='|')
+        f_reader = csv.reader(fd, delimiter=',')
 
-        print('\t'.join(next(iris_reader)))
+        print('\t'.join(next(f_reader)))
 
-        for row in iris_reader:
+        for row in f_reader:
             print('\t\t'.join(row))
     return
 
-def read_row(file, n):
+#Reads a specified row in a csv file.
+#file:  csv file name.
+#n:     Row number.
+def print_row(file, n):
     with open(file, newline='') as fd:
-        iris_reader = csv.reader(fd, delimiter=',', quotechar='|')
+        f_reader = csv.reader(fd, delimiter=',')
 
         for i in range(n):
-            next(iris_reader)
+            next(f_reader)
         
-        print('\t\t'.join(next(iris_reader)))
+        print('\t\t'.join(next(f_reader)))
 
     return
 
-def add_item(file, s_length, s_width, p_length, p_width, species):
+#Appends an entry to the end of a given csv file.
+#file:      csv file name.
+#*values:   Any number of values to be placed in the columns.
+def add_row(file, *values):
+    with open(file, newline='') as fd:
+        reader = csv.reader(fd, delimiter=',')
+        length = len(next(reader))
+
     with open(file, 'a', newline='') as fd:
-        iris_reader = csv.reader(fd, delimiter=',', quotechar='|')
+        reader = csv.reader(fd, delimiter=',')
 
-        new = str(s_length) + ',' + str(s_width) + ',' + str(p_length) + ',' + str(p_width) + ',' + species + '\n'
+        new = '\n'
 
+        if(len(values) != length):
+            print('Number of entries does not match')
+            return
+
+        for value in values:
+            new = new + str(value)
+            if(value != values[-1]):
+                new = new + ','
+        
         fd.write(new)
     return
 
-def del_item(file, n):
-    if(n == 0):
-        print('Do not attempt to delete the first row.')
-        return
+#Appends an entry to the end of a given csv file (given as a series of lists).
+#file:      csv file name.
+#*items:    Any number of items to be appended into the csv file. 
+#           Remember to only use the same number of columns per entry.
+def add_rows(file, *items):
+    for item in items:
+        add_row(file, *item)
 
-    i = 0
-    items = []
-    
+#Deletes one or several rows in a given csv file.
+#file:  csv file name.
+#n:     Row to delete.
+def del_row(file, *n):
+    for i in n:
+        if(i <= 0):
+            print('invalid entry')
+            return
+
     with open(file, newline='') as fd:
-        iris_reader = csv.reader(fd, delimiter=',', quotechar='|')
-        done = False
-        for row in iris_reader:
-            if(not done and i == n):
-                done = True
-            else:
-                items.append(row)
-            i = i+1
-            
+        f_dict_reader = csv.DictReader(fd)
+        items = []
+        for rows in f_dict_reader:
+            items.append(rows)
 
-    with open(file, 'w', newline='') as fd:
-        iris_writer = csv.writer(fd, delimiter=',', quotechar='|')
-        for j in items:
-            iris_writer.writerow(j)
+    remove = []
 
-    print(items)
-
+    for i in n:
+        remove.append(items[i-1])
+    
+    items = [x for x in items if x not in remove]
+    
+    with open(file, 'w', newline ='') as fd:
+        f_writer = csv.writer(fd)
+        f_writer.writerow(items[0])
+        for item in items:
+            f_writer.writerow(item.values())
     return
 
-read_items('iris.csv')
-read_row('iris.csv', 5)
-add_item('iris.csv', 1, 2, 3, 4, 'bepis')
+print('All entries:')
+print_rows('iris.csv')
+
+a = 5
+print('\nEntry on row ' + str(a) + ':')
+print_row('iris.csv', a)
+
+#add_item('iris.csv', 1, 2, 3, 4, 'bepis')
+#add_items('iris.csv', [1, 2, 3, 4, 'bepis'], [5, 6, 7, 8, 'bepis2'])
+
+#del_row('iris.csv', 1, 2, 3)
